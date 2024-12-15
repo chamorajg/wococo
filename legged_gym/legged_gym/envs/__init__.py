eee9a7e4590501c31d1ae9abd12fbb728bcbb93f
KeWang1017 <ke.wang@helloleap.ai>
2024-12-15 15:30:49 +0000

    change the effort

--- legged_gym/legged_gym/envs/__init__.py
+++ legged_gym/legged_gym/envs/__init__.py
@@ -5,6 +5,7 @@ from .h1.h1_jumpjack_config import H1JumpJackCfg, H1JumpJackCfgPPO
 from .gpr.gpr_jumpjack_config import GPRJumpJackCfg, GPRJumpJackCfgPPO
 from .base.legged_robot import LeggedRobot
 from .base.gpr_jumpjack import GPRJumpJack
+from .base.h1_jumpjack import H1JumpJack
 from legged_gym.utils.task_registry import task_registry
 
 task_registry.register( "h1:jumpjack", H1JumpJack, H1JumpJackCfg(), H1JumpJackCfgPPO())
